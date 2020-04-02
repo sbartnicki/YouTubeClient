55840f3de216bb900ba2175784343b81efc00ded
Sly <sylwesterbartnicki@gmail.com>
2020-04-02 20:10:14 +0200

    API setup completed, tested

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,7 @@ class SearchBar extends React.Component {
   onFormSubmit = e => {
     e.preventDefault()
 
-    //todo: make sure we callback from parent component
+    this.props.onFormSubmit(this.state.term)
   }
 
   render() {
