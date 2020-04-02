fee5931222d70d873eb4c7892bea982d062b5293
Sly <sylwesterbartnicki@gmail.com>
2020-04-02 19:36:40 +0200

    axios setup

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,2 +1,12 @@
+import axios from 'axios'
+
 const KEY = 'AIzaSyADFfpo6yqwtkqdsK0qkQaGCPrmn0aM4rU'
 
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+})
