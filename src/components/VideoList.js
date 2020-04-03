4d7ec23f7216667ff4969a98977dd4a04fb9a51f
Sly <sylwesterbartnicki@gmail.com>
2020-04-03 12:24:31 +0200

    Video list added

---
+++ src/components/VideoList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import VideoItem from './VideoItem'
+
+const VideoList = ({ videos }) => {
+  return (
+    <div className="ui relaxed divided list">
+      {videos.map(video => {
+        return <VideoItem video={video} />
+      })}
+    </div>
+  )
+}
+
+export default VideoList
